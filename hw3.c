898213bebb17d555e2403659b4ec57ad611f1aa1
sshah233 <35814079+sshah233@users.noreply.github.com>
2018-10-18 14:13:19 -0500

    Update hw3.c

--- hw3.c
+++ hw3.c
@@ -15,14 +15,14 @@ void sig_handler(int sig)
 {
 	char msg[] = " SIGINT Handled.\nCS361>";
 	write(1, msg, sizeof(msg));
-	exit(0);
+	//exit(0);
 }
 
 void sig_handler1(int sig)
 {
 	char msg[] = " SIGTSTP Handled.\nCS361>";
 	write(1, msg, sizeof(msg));
-	exit(0);
+	//exit(0);
 }
 
 
@@ -33,9 +33,11 @@ int main()
 	signal(SIGTSTP, sig_handler1);
 	
 	char line [500];			//create space
-	char argsarr[20][100] = {"/bin/ls", "-l", (char) 0};
-	//char buf1[50];
-	//char buf2[50];
+	char *argsarr[20];
+	argsarr[0] = "/bin/ps ";
+	argsarr[1] =  (char *)0;
+	//char *argsarr[21] = {"/bin/ls ", "-l", (char *)0};
+	
 
 	while (1)
 	{
@@ -57,56 +59,40 @@ int main()
         	while(word)
         	{
                 	printf("word: %s\n", word);
-                	strcpy(argsarr[i], word);     //copy word to array
+			argsarr[i] = word;		//copy word to array
                 	word = strtok(NULL, " ");	//to get next word
                 	i = i + 1;
                 }
         	
-		
-        	//print array
-        	int j=0;
+		//print array
+		int j=0;
 		
         	for (;j<i;j++)
 		{
                 	printf("argsarr[%d]: %s\n", j, argsarr[j]);
 
 		}
-		//args[j] = NULL;
-        	        
+
+        	        	        
         	int pid = fork();
         	if(pid == 0)
       		{
-         		int file1 = open("sample1.txt", O_RDONLY);
-			int file2 = open("sample2.txt", O_RDONLY);
-			
-			/*
-			read(file1,buf1,19);
-			buf1[19] = '\0';
-			printf("%s\n",buf1);
-
-			read(file2,buf2,19);
-			buf2[19] = '\0';
-			printf("%s\n",buf2);
-
-			dup2(file1,file2);
-			*/
-
-			printf("PID : %d ", getpid());		//Child's PID
-                	exit(0);
+			printf("PID : %d", getpid());		//Child's PID
+                	execv(argsarr[0], argsarr);
+			exit(0);
         	}
 
         	else
 		{
-			//printf("I am the parent.  I am waiting for my child %d to die.\n", pid);
-                	int status;
+			int status;
+			printf(" status number %d\n", status);
                 	wait(&status);
-             		printf(" Status: %d. \n");		//Child's exit status
+             		printf(" Status: %d. \n", status);		//Child's exit status
 
-        	}	
-
+        	}
 	}
 	
-	//return 0;
+	return 0;
 }
 
 
