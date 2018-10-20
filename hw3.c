15c19d914082d72212fbaa9a0f8e8fe647051d3b
sshah233 <35814079+sshah233@users.noreply.github.com>
2018-10-19 19:38:39 -0500

    Update hw3.c

--- hw3.c
+++ hw3.c
@@ -1,11 +1,15 @@
+//Name : Shruti Shah
+// Note: I have used lab 4-5 codes provided my the professor to do this homework. 
+
 #include <stdio.h>
 #include <string.h>
-#include <unistd.h>
 #include <signal.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+
 
 void sig_handler(int sig)
 {
@@ -22,101 +26,93 @@ void sig_handler1(int sig)
 }
 
 
-int main(){
-  
-  signal(SIGINT, sig_handler);
-  signal(SIGTSTP, sig_handler1);
 
-  //create some space for our strings
-  char line[500];
-  char *argsarr[21];
-  char* exitLine = "exit";
-  int redirect=0;
-  
- while(1){
-  //print prompt
-  printf("CS361 > ");
-  //read line from terminal
-  fgets(line, 500, stdin);
-  line[strcspn(line, "\n")] = 0;
-  //break the string up into words
-  if(strcmp(line,"exit")==0){
-	exit(0);
-  }
-  char *word = strtok(line, " ");
-  int i = 0;
-// char *filename = (char *)malloc(sizeof(char) * 50);
-  while (word) {
-      if((strcmp(word, ">") == 0) || (strcmp(word, "<") == 0) || (strcmp(word, ";") == 0))
-        {
-           if(strcmp(word, ">") == 0){
-                redirect = 1;
-	   	}
-           if(strcmp(word, "<") == 0){
-                redirect = 2;
-	  }
-           if(strcmp(word, ";") == 0){
-                redirect = 3;
-          }
-           word = strtok(NULL, " ");
-                                // copy a word to the filename
-           //strcpy(filename, word);
-           //word = strtok(NULL, " ");
-                        }
-     else{
-    printf("word: %s\n", word);
-    //copy a word to the arg array
-    argsarr[i]=word;
-    //get next word
-    word = strtok(NULL, " ");
-    i = i + 1;
-  }
+int main()
+{
+	signal(SIGINT, sig_handler);
+	signal(SIGTSTP, sig_handler1);
+	
+	char line [500];			//create space
+	//char *argsarr[20];
+	//argsarr[0] = "/bin/ps ";
+	//argsarr[1] =  (char *)0;
+	char *argsarr[21] = {"/bin/ls ", "-l", (char *)1};
+	
 
-  //print out our array
+	while (1)
+	{
+		printf("CS361 >");		//it prints the prompt
+		
+        	//read line from terminal
+        	fgets(line,500,stdin);
+		line[strcspn(line, "\n")] = 0;
+		
+        	//break string into words
+        	char *word = strtok(line, " ");
+        	int i = 0;
+			
+		if(strcmp(line,"exit")==0){
+			exit(0);
+		}
 
-  argsarr[i] = NULL;
-}
- int pid = fork();
- 
-  if (pid == 0) {
-    int line_num;
-    if(redirect==1){
-	line_num = open(filename,O_RDWR |O_CREAT|O_TRUNC, S_IRUSR|S_IWUSR|S_IROTH);
-	dup2(line_num,1);
-        close(line_num);
-    }
-    if(redirect==2){
-	line_num = open(filename,O_WRONLY | O_APPEND);
-	dup2(line_num,1);
-	close(line_num);
-    }
-    if(redirect==3){
-	line_num = open(filename, O_RDONLY);
-	if(line_num<0){
+
+        	while(word)
+        	{
+			/* if(strcmp(word, ">") == 0){
+               			 redirect = 1;
+	   		}
+          		else if(strcmp(word, "<") == 0){
+             			   redirect = 2;
+	 		 }
+          		else if(strcmp(word, ";") == 0){
+               			 redirect = 3;
+        		  }*/
+
+                	printf("word: %s\n", word);
+			argsarr[i] = word;		//copy word to array
+                	word = strtok(NULL, " ");	//to get next word
+                	i = i + 1;
+                }
+
+		//print array
+		int j=0;
+		
+        	for (;j<i;j++)
+		{
+                	printf("argsarr[%d]: %s\n", j, argsarr[j]);
+
+		}
+		
+        	        	        
+        	int pid = fork();
+        	if(pid == 0)
+      		{	
+
+			//printf("pid:%d", getpid());		
+                	execv(argsarr[0], argsarr);
+			exit(0);
+        	}
+
+        	else
+		{
+			int status;
+			//printf(" status number %d\n", status);
+                	//wait(status);
+			waitpid(-1,&status,0);
+			int ex = WEXITSTATUS(status);
+             		printf("pid:%d status:%d\n",pid,ex);		
+
+        	}
+		
+		
+
 	}
-	else{
-	dup2(line_num,0);
-	close(line_num);
-        }
-    }
-    printf("pid:%d\n", getpid());
-    execvp(argsarr[0], argsarr);
-    exit(0);
-  } 
-  else 
-   {
-    int status;
-    //printf(" status number %d\n", status);
-    //wait(status);
-     waitpid(-1,&status,0);
-     int ex = WEXITSTATUS(status);
-     printf("pid:%d status:%d\n",pid,ex);
-   }  
+	
+	return 0;
+}
 
-  
-}
- return 0;
-}
+
+
 
 
 
